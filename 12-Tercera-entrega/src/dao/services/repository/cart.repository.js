f9fcdd9586385f5c05f66d12de91175fe78fd85b
Aldair González <aldairgome97@gmail.com>
2023-04-05 16:40:28 -0600

    Tercera pre entrega del PF v1

---
+++ 12-Tercera-entrega/src/dao/services/repository/cart.repository.js
@@ -0,0 +1,45 @@
+export class CartRepository {
+  constructor (dao) {
+    this.dao = dao
+  }
+
+  getAll () {
+    const result = this.dao.getAll()
+    return result
+  }
+
+  getOne (cid) {
+    const result = this.dao.getOne(cid)
+    return result
+  }
+
+  create () {
+    const result = this.dao.create()
+    return result
+  }
+
+  update (cid, obj) {
+    const result = this.dao.update(cid, obj)
+    return result
+  }
+
+  delete (cid) {
+    const result = this.dao.delete(cid)
+    return result
+  }
+
+  productAdd (cid, pid) {
+    const result = this.dao.productAdd(cid, pid)
+    return result
+  }
+
+  productUpdate (cid, pid, obj) {
+    const result = this.dao.productUpdate(cid, pid, obj)
+    return result
+  }
+
+  productRemove (cid, pid) {
+    const result = this.dao.productRemove(cid, pid)
+    return result
+  }
+}
